93237d4812d4081569ba83b9f63c1ae3edf532e5
Achilles G <achillesgaikwad@gmail.com>
2016-04-15 14:20:06 +0530

    Update and rename dinner.py to dining_philosophers.py

--- dining_philosophers.py
+++ dining_philosophers.py
@@ -2,27 +2,23 @@ import threading
 import random
 import time
 from pymongo import MongoClient
-
+# TODO : USE SIMULATOR TO GATHER DATA VIA WSN  
 # create connection
-'''
+
 client = MongoClient()
 
 client = MongoClient('localhost',27017)
 db = client.test_database
-db = client['test-database']
+db = client['philosopher']
 collection = db.test_collection
 posts = db.posts
 
 
-
-
-'''
-#queue is actually a stack, will change the variable name later. 
-myQueue = []
+myStack = []
 class Philosopher(threading.Thread):
 
     running = True
-    global myQueue
+    global myStack
     global posts
     def __init__(self,xname,forkOnLeft,forkOnRight):
         threading.Thread.__init__(self)
@@ -33,7 +29,7 @@ class Philosopher(threading.Thread):
 
     def run(self):
         while(self.running):
-            if(len(myQueue)!=0):
+            if(len(myStack)!=0):
                 time.sleep(random.uniform(3,13))
                 print '%s is hungry.' %self.name
                 self.dine()
@@ -60,16 +56,18 @@ class Philosopher(threading.Thread):
 
     def dinning(self):
         try:
-            self.kya_khara = myQueue.pop()
+            self.kya_khara = myStack.pop()
             print '%s starts eating ' % self.name, self.kya_khara, '\n'
             foo = {"Philosopher": self.name,
                    "Food Item" : self.kya_khara}
             time.sleep(random.uniform(1, 10))
             print '%s finishes eating and leaves to think' % self.name
-            #try:
-            #    post_id= posts.insert_one(foo).inserted_id
-            #except:
-            #    pass
+            try:
+                post_id= posts.insert_one(foo).inserted_id
+                print post_id
+            except:
+                print 'foo '
+                pass
 
         except IndexError:
             print '\n %s has nothing to Eat' %self.name
@@ -77,15 +75,15 @@ class Philosopher(threading.Thread):
 
 
 def DiningPhilosophers():
-    global myQueue
+    global myStack
     forks = [threading.Lock() for n in range(5)]
     philosopherNames =('Chutiya','Bulla','Pappu Pager','Lamboo Aata','Ibu Hatela')
 
     foodItems = ('vadapav','meduvada','dosa','idli sambar','pohe with peas','samosa','burger','pizza')
 
     for i in range (random.randint(10,20)):
-        myQueue.append(foodItems[random.randint(0,len(foodItems)-1)])
-    print 'Food items available: ',myQueue
+        myStack.append(foodItems[random.randint(0,len(foodItems)-1)])
+    print 'Food items available: ',myStack
 
     philosophers = [Philosopher(philosopherNames[i],forks[i%5],forks[(i+1)%5]) for i in range(5)]
 
